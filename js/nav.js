5918acfd11d934b019feb004fc728c8aadae76c9
thecomediandan <danishpurplesnake@gmail.com>
2024-03-05 23:38:27 -0400

    Se termino de maquetar la parte visual con los estilos y algunas animaciones

--- js/nav.js
+++ js/nav.js
@@ -4,123 +4,151 @@ const option3 = document.querySelector(".nav-options--button-3");
 const option4 = document.querySelector(".nav-options--button-4");
 
 const buttonTheme = document.querySelector(".div-setux--buttom-theme");
-const buttonLanguage = document.querySelector(".div-setux--buttom-language");
+const buttonDarkTheme = document.querySelectorAll('.div-setux--buttom-theme span')[0];
+const buttonLightTheme = document.querySelectorAll('.div-setux--buttom-theme span')[1];
+const buttonLanguageSpanish = document.querySelectorAll(".menu-language button")[0];
+const buttonLanguageEnglish = document.querySelectorAll(".menu-language button")[1];
+const personalName = document.querySelector('.wave-name');
 const areaMenuTheme = document.querySelector(".menu-theme");
 const areaMenuLanguage = document.querySelector(".menu-language");
-const menus = document.querySelectorAll(".setux-option--menu");
-const buttonAuto = document.querySelector(".setux-option--menu-auto");
-const buttonNormal = document.querySelector(".setux-option--menu-normal");
-const buttonDark = document.querySelector(".setux-option--menu-dark");
-const buttonEnglish = document.querySelector(".setux-option--menu-english");
-const buttonSpanish = document.querySelector(".setux-option--menu-spanish");
+// const menus = document.querySelectorAll(".setux-option--menu");
+// const buttonAuto = document.querySelector(".setux-option--menu-auto");
+// const buttonNormal = document.querySelector(".setux-option--menu-normal");
+// const buttonDark = document.querySelector(".setux-option--menu-dark");
+// const buttonEnglish = document.querySelector(".setux-option--menu-english");
+// const buttonSpanish = document.querySelector(".setux-option--menu-spanish");
+
+// ? Animation name
+personalName.addEventListener('mouseenter', (e) => {
+    const grettings = document.querySelector('.wave');
+    grettings.classList.toggle('wave-from-name');
+});
+personalName.addEventListener('mouseout', (e) => {
+    const grettings = document.querySelector('.wave');
+    grettings.classList.toggle('wave-from-name');
+});
 
 
 // Click event of options buttons
-option1.addEventListener("click", (e) => {
-    if (!option1.classList.contains("nav-options--button-clicked")) {
-        cleanClickedOptions("nav-options--button-clicked");
-        option1.classList.add("nav-options--button-clicked");
-    }
-});
-option2.addEventListener("click", (e) => {
-    if (!option2.classList.contains("nav-options--button-clicked")) {
-        cleanClickedOptions("nav-options--button-clicked");
-        option2.classList.add("nav-options--button-clicked");
-    }
-});
-option3.addEventListener("click", (e) => {
-    if (!option3.classList.contains("nav-options--button-clicked")) {
-        cleanClickedOptions("nav-options--button-clicked");
-        option3.classList.add("nav-options--button-clicked");
-    }
-});
-option4.addEventListener("click", (e) => {
-    if (!option4.classList.contains("nav-options--button-clicked")) {
-        cleanClickedOptions("nav-options--button-clicked");
-        option4.classList.add("nav-options--button-clicked");
-    }
-});
+// option1.addEventListener("click", (e) => {
+//     if (!option1.classList.contains("nav-options--button-clicked")) {
+//         cleanClickedOptions("nav-options--button-clicked");
+//         option1.classList.add("nav-options--button-clicked");
+//     }
+// });
+// option2.addEventListener("click", (e) => {
+//     if (!option2.classList.contains("nav-options--button-clicked")) {
+//         cleanClickedOptions("nav-options--button-clicked");
+//         option2.classList.add("nav-options--button-clicked");
+//     }
+// });
+// option3.addEventListener("click", (e) => {
+//     if (!option3.classList.contains("nav-options--button-clicked")) {
+//         cleanClickedOptions("nav-options--button-clicked");
+//         option3.classList.add("nav-options--button-clicked");
+//     }
+// });
+// option4.addEventListener("click", (e) => {
+//     if (!option4.classList.contains("nav-options--button-clicked")) {
+//         cleanClickedOptions("nav-options--button-clicked");
+//         option4.classList.add("nav-options--button-clicked");
+//     }
+// });
 
 // Buttons setux hover arrow-down
-buttonTheme.addEventListener("mouseover", (e) => {
-    document.querySelector(".div-setux--buttom-theme span")
-    .setAttribute("style", "color: var(--color-light-primary1)");
-});
-buttonTheme.addEventListener("mouseout", (e) => {
-    document.querySelector(".div-setux--buttom-theme span")
-    .setAttribute("style", "color: var(--color-primary1)");
-});
-buttonLanguage.addEventListener("mouseover", (e) => {
-    document.querySelector(".div-setux--buttom-language span")
-    .setAttribute("style", "color: var(--color-light-primary1)");
-});
-buttonLanguage.addEventListener("mouseout", (e) => {
-    document.querySelector(".div-setux--buttom-language span")
-    .setAttribute("style", "color: var(--color-primary1)");
-});
+// buttonTheme.addEventListener("mouseover", (e) => {
+//     document.querySelector(".div-setux--buttom-theme span")
+//     .setAttribute("style", "color: var(--color-light-primary1)");
+// });
+// buttonTheme.addEventListener("mouseout", (e) => {
+//     document.querySelector(".div-setux--buttom-theme span")
+//     .setAttribute("style", "color: var(--color-primary1)");
+// });
+// buttonLanguage.addEventListener("mouseover", (e) => {
+//     document.querySelector(".div-setux--buttom-language span")
+//     .setAttribute("style", "color: var(--color-light-primary1)");
+// });
+// buttonLanguage.addEventListener("mouseout", (e) => {
+//     document.querySelector(".div-setux--buttom-language span")
+//     .setAttribute("style", "color: var(--color-primary1)");
+// });
 
 // Menu and buttons setux hover
-buttonTheme.addEventListener("click", (e) => {
-    let menu = document.querySelector(".menu-theme .setux-option--menu");
-    menu.setAttribute("style", "display: flex;");
-});
-buttonLanguage.addEventListener("click", (e) => {
-    let menu = document.querySelector(".menu-language .setux-option--menu");
-    menu.setAttribute("style", "display: flex;");
-});
-areaMenuTheme.addEventListener("mouseleave", (e) => {
-    let menu = document.querySelector(".menu-theme .setux-option--menu");
-    menu.setAttribute("style", "display: none;");
-});
-areaMenuLanguage.addEventListener("mouseleave", (e) => {
-    let menu = document.querySelector(".menu-language .setux-option--menu");
-    menu.setAttribute("style", "display: none;");
-});
-menus.forEach((menu) => {
-    menu.addEventListener("click", (e) => {
-        menu.setAttribute("style", "display: none;");
-    });
-});
+// buttonTheme.addEventListener("click", (e) => {
+//     let menu = document.querySelector(".menu-theme .setux-option--menu");
+//     menu.setAttribute("style", "display: flex;");
+// });
+// buttonLanguage.addEventListener("click", (e) => {
+//     let menu = document.querySelector(".menu-language .setux-option--menu");
+//     menu.setAttribute("style", "display: flex;");
+// });
+// areaMenuTheme.addEventListener("mouseleave", (e) => {
+//     let menu = document.querySelector(".menu-theme .setux-option--menu");
+//     menu.setAttribute("style", "display: none;");
+// });
+// areaMenuLanguage.addEventListener("mouseleave", (e) => {
+//     let menu = document.querySelector(".menu-language .setux-option--menu");
+//     menu.setAttribute("style", "display: none;");
+// });
+// menus.forEach((menu) => {
+//     menu.addEventListener("click", (e) => {
+//         menu.setAttribute("style", "display: none;");
+//     });
+// });
 
 // Click event of setux buttons
-buttonNormal.addEventListener("click", (e) => {
-    if (!buttonNormal.classList.contains("setux-option--menu-theme-selected")) {
-        cleanClickedOptions("setux-option--menu-theme-selected");
-        buttonNormal.classList.add("setux-option--menu-theme-selected");
-
+buttonTheme.addEventListener("click", (e) => {
+    if (!buttonDarkTheme.classList.contains("theme-desactivated")) {
+        buttonDarkTheme.classList.add("theme-desactivated");
+        buttonLightTheme.classList.remove("theme-desactivated");
         funLightMode();
-    }
-});
-buttonDark.addEventListener("click", (e) => {
-    if (!buttonDark.classList.contains("setux-option--menu-theme-selected")) {
-        cleanClickedOptions("setux-option--menu-theme-selected");
-        buttonDark.classList.add("setux-option--menu-theme-selected");
-
+    }else{
+        buttonDarkTheme.classList.remove("theme-desactivated");
+        buttonLightTheme.classList.add("theme-desactivated");
         funDarkMode();
+    }
+});
+// buttonNormal.addEventListener("click", (e) => {
+//     if (!buttonNormal.classList.contains("setux-option--menu-theme-selected")) {
+//         cleanClickedOptions("setux-option--menu-theme-selected");
+//         buttonNormal.classList.add("setux-option--menu-theme-selected");
 
-        // if (root.style.getPropertyValue('--color-light-primary1') === 'blue') to original value
-        // if (getComputedStyle(root).getPropertyValue('--color-primary').trim() === 'blue') to exact value
+//         funLightMode();
+//     }
+// });
+// buttonDark.addEventListener("click", (e) => {
+//     if (!buttonDark.classList.contains("setux-option--menu-theme-selected")) {
+//         cleanClickedOptions("setux-option--menu-theme-selected");
+//         buttonDark.classList.add("setux-option--menu-theme-selected");
+
+//         funDarkMode();
+
+//         // if (root.style.getPropertyValue('--color-light-primary1') === 'blue') to original value
+//         // if (getComputedStyle(root).getPropertyValue('--color-primary').trim() === 'blue') to exact value
+//     }
+// });
+
+buttonLanguageSpanish.addEventListener("click", (e) => {
+    if (!buttonLanguageSpanish.classList.contains("language-activated")) {
+        buttonLanguageSpanish.classList.add("language-activated");
+        buttonLanguageEnglish.classList.toggle("language-activated");
+        // TO DO 
+    }
+
+});
+buttonLanguageEnglish.addEventListener("click", (e) => {
+    if (!buttonLanguageEnglish.classList.contains("language-activated")) {
+        buttonLanguageEnglish.classList.add("language-activated");
+        buttonLanguageSpanish.classList.toggle("language-activated");
+        // TO DO
     }
 });
 
-buttonEnglish.addEventListener("click", (e) => {
-    if (!buttonEnglish.classList.contains("setux-option--menu-language-selected")) {
-        cleanClickedOptions("setux-option--menu-language-selected");
-        buttonEnglish.classList.add("setux-option--menu-language-selected");
-    }
-});
-buttonSpanish.addEventListener("click", (e) => {
-    if (!buttonSpanish.classList.contains("setux-option--menu-language-selected")) {
-        cleanClickedOptions("setux-option--menu-language-selected");
-        buttonSpanish.classList.add("setux-option--menu-language-selected");
-    }
-});
 
-
-function cleanClickedOptions(selected) {
-    let button = document.querySelector("." + selected);
-    button.classList.remove(selected);
-}
+// function cleanClickedOptions(selected) {
+//     let button = document.querySelector("." + selected);
+//     button.classList.remove(selected);
+// }
 
 // * RESPONSIVE NAV CONFIGURATION
 
@@ -148,6 +176,7 @@ buttonOptionResponsive.addEventListener("click", (e) => {
 function funDarkMode() {
     const root = document.documentElement;
     const logo = document.querySelector(".nav-logo > img");
+    const footerLogo = document.querySelector(".footer-logo > img");
     const banner = document.querySelector(".paragraph");
     const iconThemeDarkResponsive = document.querySelector(".nav--bar-icon-theme-dark");
     const iconThemeLightResponsive = document.querySelector(".nav--bar-icon-theme-light");
@@ -163,11 +192,13 @@ function funDarkMode() {
     root.style.setProperty('--color-secundary2', 'var(--color-dark-secundary2)');
     root.style.setProperty('--color-accent', 'var(--accent-dark)');
     logo.setAttribute("src", "./img/logo/Logotipo-dark.png");
-    banner.setAttribute("style", "background-image: url('../img/logo/Fondo-Dark.png');");
+    footerLogo.setAttribute("src", "./img/logo/Logotipo-dark.png");
+    banner.setAttribute("style", "background-image: url('../img/logo/Fondo-Darkv2.png');");
 }
 function funLightMode() {
     const root = document.documentElement;
     const logo = document.querySelector(".nav-logo > img");
+    const footerLogo = document.querySelector(".footer-logo > img");
     const banner = document.querySelector(".paragraph");
     const iconThemeDarkResponsive = document.querySelector(".nav--bar-icon-theme-dark");
     const iconThemeLightResponsive = document.querySelector(".nav--bar-icon-theme-light");
@@ -182,19 +213,20 @@ function funLightMode() {
     root.style.setProperty('--color-secundary2', 'var(--color-light-secundary2)');
     root.style.setProperty('--color-accent', 'var(--accent-light)');
     logo.setAttribute("src", "./img/logo/Logotipo-light.png");
-    banner.setAttribute("style", "background-image: url('../img/logo/Fondo-Light.png');");
+    footerLogo.setAttribute("src", "./img/logo/Logotipo-light.png");
+    banner.setAttribute("style", "background-image: url('../img/logo/Fondo-Lightv2.png');");
 }
 
 var AutoModeThemeDark;
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    console.log("Dark Mode detected...")
-    buttonDark.classList.add("setux-option--menu-theme-selected");
+    console.log("Theme: Dark Mode detected...")
     AutoModeThemeDark = true;
     funDarkMode();
 }else {
-    console.log("Light Mode detected...")
-    buttonNormal.classList.add("setux-option--menu-theme-selected");
+    console.log("Theme: Light Mode detected...")
     AutoModeThemeDark = false;
+    buttonDarkTheme.classList.add("theme-desactivated");
+    buttonLightTheme.classList.remove("theme-desactivated");
     funLightMode();
 }
 
