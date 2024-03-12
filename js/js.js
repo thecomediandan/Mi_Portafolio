dbe135bc43b51a26f4dc286c8b90beaff7778a8d
thecomediandan <danishpurplesnake@gmail.com>
2024-03-12 16:05:00 -0400

    Se inicio la base para el cambio de lenguaje español-ingles

---
+++ js/js.js
@@ -0,0 +1,38 @@
+  const lanSpanish = document.querySelector(".div-setux--buttom-language--spanish");
+  const lanEnglish = document.querySelector(".div-setux--buttom-language--english");
+
+  lanSpanish.addEventListener("click", function() {
+    cargarTraducciones("es");
+  });
+
+  lanEnglish.addEventListener("click", function() {
+    cargarTraducciones("en");
+  });
+  
+  function cargarTraducciones(idioma) {
+    // Realiza una solicitud AJAX para cargar el archivo de idioma correspondiente
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "./../content/config/" + idioma + ".json", true);
+    xhr.onload = function() {
+      if (xhr.status == 200) {
+        var traducciones = JSON.parse(xhr.responseText);
+        aplicarTraducciones(traducciones);
+      }
+    };
+    xhr.send();
+  }
+  
+  function aplicarTraducciones(traducciones) {
+    // Itera sobre las traducciones y reemplaza el contenido en la página
+    for (var clave in traducciones) {
+      if (traducciones.hasOwnProperty(clave)) {
+        console.log(clave)
+        var elemento = document.querySelector("[data-traduction='" + clave + "']");
+        console.log(elemento)
+        if (elemento) {
+          elemento.textContent = traducciones[clave];
+        }
+      }
+    }
+  }
+  
